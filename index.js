c10b2a66f295310304bec3d051ef0a75e1e85ea5
SoMei Lam <lamsomeiamy@gmail.com>
2021-07-14 10:46:45 -0700

    Finished task 6&7

--- index.js
+++ index.js
@@ -134,10 +134,16 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+function removeFlavorByName(array, flavour){
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === flavour) {
+      array.splice(i, 1);
+    }
+  }
+  return array;
 }
 
+console.log('task 6 ', removeFlavorByName(originalFlavors,  "Rocky Road"));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -160,9 +166,20 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+function filterByWord(array, string){
+  // set up the new array that we want to push our results to
+  const filteredArray = [];
+  // loop through the entire array and check each index to see if it includes a string
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].includes(string)) {
+      // if it does include the string we want to push it to our filtered array
+      filteredArray.push(array[i]);
+    }
+  }
+  return filteredArray;
 }
+
+console.log('task 7 ', filterByWord(originalFlavors, "Chocolate"));
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
